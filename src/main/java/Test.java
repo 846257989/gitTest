55a35b246f0152f4472011e4ae73170df2a58e27
Your Name <you@example.com>
2022-09-25 20:02:06 +0800

    Xiao Ming second Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git基础");
         System.out.println("XiaoMing的新分支");
+        System.out.println("Master分支");
+        System.out.println("小明的分支");
     }
 }
